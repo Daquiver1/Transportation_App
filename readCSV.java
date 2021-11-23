cd3471848c3b45d1977da770708cdb53301b0394
Christian <cabrokwa11@gmail.com>
2021-11-23 03:28:28 +0000

    cleaned the file

--- readCSV.java
+++ readCSV.java
@@ -1,4 +1,4 @@
-package csv;
+
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
@@ -10,13 +10,12 @@ import java.util.*;
 public class readCSV {
 
     public static void main(String[] args) throws IOException, CsvException {
-        String source = "School of Pharmacy";
-        String destination = "Faculty of arts";
+        String source = "School of Law Legon";
+        String destination = "Commonwealth Hall Legon";
 
-        String filename = "C:\\Users\\Anita Agyepong\\Documents\\Daquiver's Quivers\\Java\\JavatarDemo\\dummy_data.csv";
+        String filename = "Scrapper/Addresses.csv";
 
         List<String[]> r;    // this is a list because reader.readAll returns a list.
-
         try (CSVReader reader = new CSVReader(new FileReader(filename))){
             r = reader.readAll();
         }
@@ -49,109 +48,88 @@ public class readCSV {
         System.out.println(column_end_index);
 
         // variables
-        double total_distance = 0;
+        double actual_distance = 0;
 
         // Finding total distance
-        for (String[] array: r){
-            if (r.indexOf(array) == row_start_index){
-                for (int i = 0; i < array.length; i++){
-                    if (i == column_end_index) {
-                        total_distance = Float.parseFloat(array[i]);
-                    }
-                }
-            }
-            else {continue;}
-        }
+        actual_distance = Double.parseDouble(r.get(row_start_index)[column_end_index]);
 
         System.out.println("Source: " + source);
         System.out.println("Destination: " + destination);
-        System.out.println("Total Distance: " + total_distance);
+        System.out.println("Actual Distance: " + actual_distance);
 
         String[] destinationArray = r.get(0);    // the first row in the csv
         String[] startArray = r.get(row_start_index);
 
         // routes
         Map<String, Double> routes = new LinkedHashMap<String, Double>();
+
+        // explored landmarks
         List<String> exploredPathIndexes = new ArrayList<>();
         exploredPathIndexes.add(source);
 
-//        for (int j = 0; j< 3; j++){
-//            Double[] min_array = generatePath(startArray, total_distance, exploredPathIndexes);
-//            int minIndex = min_array[1].intValue();
-//            Double min = min_array[0];
-//
-////            if (destinationArray[minIndex] != "School of Pharmacy"){
-//            routes.put(destinationArray[minIndex], min);
-//            exploredPathIndexes.add(minIndex);
-//            startArray = r.get(minIndex);
-////            }
-////            else {
-////                break;
-////            }
-//        }
-        Double[] min_array = generatePath(startArray, destinationArray,  total_distance, exploredPathIndexes);
-        if (min_array != null) {
+        double current_distance_to_destination = actual_distance;
+        Double[] min_array = {};
+
+        for (int j = 0; j< 5; j++){
+            min_array = generatePath(r, startArray, destinationArray,  actual_distance, exploredPathIndexes, current_distance_to_destination);
             int minIndex = min_array[1].intValue();
-            Double min = min_array[0];
+            double min = min_array[0];
 
             routes.put(destinationArray[minIndex], min);
             exploredPathIndexes.add(destinationArray[minIndex]);
+            if (j == 4) {routes.put(destination, Double.parseDouble(startArray[column_end_index]));} // after 3 landmarks
             startArray = r.get(minIndex);
+            current_distance_to_destination = Double.parseDouble(startArray[column_end_index]);
         }
 
-        min_array = generatePath(startArray, destinationArray,  total_distance, exploredPathIndexes);
-        if (min_array != null) {
-            int minIndex = min_array[1].intValue();
-            Double min = min_array[0];
-
-            routes.put(destinationArray[minIndex], min);
-            exploredPathIndexes.add(destinationArray[minIndex]);
-            startArray = r.get(minIndex);
-        }
-
-        min_array = generatePath(startArray, destinationArray,  total_distance, exploredPathIndexes);
-        if (min_array != null) {
-            int minIndex = min_array[1].intValue();
-            Double min = min_array[0];
-
-            routes.put(destinationArray[minIndex], min);
-            exploredPathIndexes.add(destinationArray[minIndex]);
-            startArray = r.get(minIndex);
-        }
-
-
-//        for (String name: routes.keySet()) {
-//            String key = name.toString();
-//            String value = routes.get(name).toString();
-//            System.out.println(key + " : " + value);
-//        }
 
         System.out.println(routes);
 
+        double total_distance = 0;
+
+        System.out.print(source);
+        for (String name: routes.keySet()) {
+            String key = name.toString();
+            total_distance += routes.get(name);
+            System.out.print(" => " + key);
+        }
+        System.out.println();
+        System.out.println("Total Path Distance: " + total_distance);
+
+        int average_car_speed = 50;
+
+        float timeOfArrival = (float) total_distance / 50;
+        System.out.println("Time taken to travel path: " + timeOfArrival + " hour(s)");
     }
 
 
-    public static Double[] generatePath(String[] startArray, String[] destinationArray, Double total_distance, List<String> explored_paths){
+    public static Double[] generatePath(List<String[]> r, String[] startArray, String[] destinationArray, Double total_distance, List<String> explored_paths, Double previous_distance){
         // iterating through routes
-        Double min = 2.00;
+        double min = 2.00;
         int minIndex = 0;
         Double[] minArray = new Double[2];
 
         for(int i = 1; i< startArray.length; i++){
-            if (Double.parseDouble(startArray[i]) < min && Double.parseDouble(startArray[i]) != 0) {
-                min = Double.parseDouble(startArray[i]);
-                minIndex = i;
+            double i_ = Double.parseDouble(startArray[i]);
+            if (i_ < min && i_ != 0) {
+                // if the min, i < distance_of_previous_landmark from destination
+                if (Double.parseDouble(r.get(i)[5]) < previous_distance){
+                    min = Double.parseDouble(startArray[i]) + 0.1; //We can add some values to this to get different routes.// I added 0.1 so it will ignore routes which are very close to it. 
+                    minIndex = i;
+                }
 
+                // ignoring already explored landmarks.
                 for (String location: explored_paths){
-                    if (destinationArray[minIndex].contains(location)){
+                    if (destinationArray[minIndex].contains(location)) {
                         min = 22.00;
+                        break;
                     }
                 }
 
             }
         }
         minArray[0] = min;
-        minArray[1] = Double.valueOf(minIndex);
+        minArray[1] = (double) minIndex;
         return minArray;
     }
 }
